316c9e00f023cb7924c5b25b0c94d45cfdfd2a63
Jourdan Rodrigues <thiagojourdan@gmail.com>
2018-09-16 23:16:02 -0300

    :sparkles: Add bill event concept

--- app/models/_customer.py
+++ app/models/_customer.py
@@ -9,7 +9,12 @@ from django.utils.translation import ugettext as _
 class CustomerQueryset(QuerySet):
     def that_should_be_billed(self) -> QuerySet:
         today = date.today()
-        return self.filter(billing_day__gte=today.day, billing_day__lte=today.day + 2)
+        return self.filter(
+            billing_day__gte=today.day, billing_day__lte=today.day + 2,
+        ).exclude(
+            # Customers with bill events from this month or in the future shouldn't be billed
+            bill_events__date__month__gte=today.month,
+        )
 
 
 class CustomerManager(BaseManager.from_queryset(CustomerQueryset)):
@@ -30,3 +35,8 @@ class Customer(models.Model):
 
     class BillingDayNotAllowed(Exception):
         pass
+
+
+class CustomerBillEvent(models.Model):
+    date = models.DateField(_('date'), default=date.today, editable=False)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE, related_name='bill_events')
