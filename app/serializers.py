5ebbc50c654f1f46aeaa36a4e3eda843e9362288
Jourdan Rodrigues <thiagojourdan@gmail.com>
2018-09-17 15:41:53 -0300

    :sparkles: Charge when order is closed

--- app/serializers.py
+++ app/serializers.py
@@ -1,3 +1,4 @@
+from django.db import transaction
 from rest_framework import serializers
 
 from app.models import ChocolatePreference, Order
@@ -19,6 +20,18 @@ class ChocolatePreferenceSerializer(SaveWithCustomerMixin, serializers.ModelSeri
 
 
 class OrderSerializer(SaveWithCustomerMixin, serializers.ModelSerializer):
+    def _save_and_charge(self, **kwargs):
+        with transaction.atomic():
+            super().save(**kwargs)
+
+            order = self.instance
+            order.customer.charge(order.value)
+
+    def save(self, **kwargs):
+        if self.validated_data['closed']:
+            self._save_and_charge(**kwargs)
+        else:
+            super().save(**kwargs)
 
     class Meta:
         model = Order
