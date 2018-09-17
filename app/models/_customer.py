4c11f3a89ae5535e239d8bc24e296bff43968bfd
Jourdan Rodrigues <thiagojourdan@gmail.com>
2018-09-16 22:56:55 -0300

    :sparkles: Apply custom queryset class to customer

--- app/models/_customer.py
+++ app/models/_customer.py
@@ -1,10 +1,26 @@
+from datetime import date
+
 from django.db import models
+from django.db.models import QuerySet
+from django.db.models.manager import BaseManager
 from django.utils.translation import ugettext as _
+
+
+class CustomerQueryset(QuerySet):
+    def that_should_be_billed(self) -> QuerySet:
+        today = date.today()
+        return self.filter(billing_day__gte=today.day, billing_day__lte=today.day + 2)
+
+
+class CustomerManager(BaseManager.from_queryset(CustomerQueryset)):
+    pass
 
 
 class Customer(models.Model):
     billing_day = models.PositiveSmallIntegerField(_('billing day'))
     BILLING_DAY_ALLOWED_RANGE = range(1, 28)
+
+    objects = CustomerManager()
 
     def save(self, force_insert=False, force_update=False, using=None, update_fields=None):
         if self.billing_day not in self.BILLING_DAY_ALLOWED_RANGE:
